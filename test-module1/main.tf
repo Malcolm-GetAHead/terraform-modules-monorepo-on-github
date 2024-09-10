22e860364022cb32c4add20e29c97e6da625328a
Malcolm Acker <Malcolm@GetA-Head.com>
2024-09-10 14:28:10 -0400

    Invalid provider

--- test-module1/main.tf
+++ test-module1/main.tf
@@ -2,3 +2,15 @@ resource "null_resource" "test" {
   count    = 1
   triggers = { a = "b" }
 }
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.96.0"
+    }
+  }
+}
+
+provider "aws" {
+}
