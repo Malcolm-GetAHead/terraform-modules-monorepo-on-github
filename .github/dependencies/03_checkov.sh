568d50f5c53237fced6aaa548c0d514314936bb4
Malcolm Acker <Malcolm@GetA-Head.com>
2024-09-11 10:43:58 -0400

    a

--- .github/dependencies/03_checkov.sh
+++ .github/dependencies/03_checkov.sh
@@ -2,7 +2,7 @@
 
 VERSION=3.2.250
 
-python -m env "$WS_DIR/.cache/checkov"
+python -m venv "$WS_DIR/.cache/checkov"
 "$WS_DIR/.cache/checkov/bin/pip" install "checkov==$VERSION"
 
 ln -s "$WS_DIR/.cache/checkov/bin/checkov" "${WS_DIR}/.local/bin"
