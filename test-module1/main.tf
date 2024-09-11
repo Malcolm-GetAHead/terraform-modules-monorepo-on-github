efcc58e8d2d7a7a54db4a5d2d994a9763cfa7a66
Malcolm Acker <Malcolm@GetA-Head.com>
2024-09-11 10:53:28 -0400

    Fix init error

--- test-module1/main.tf
+++ test-module1/main.tf
@@ -3,19 +3,5 @@ resource "null_resource" "test" {
   triggers = { a = "b" }
 }
 
-terraform {
-  required_version = "~> 1.9.0"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.60.0"
-    }
-    null = {
-      source  = "hashicorp/null"
-      version = "3.2.2"
-    }
-  }
-}
-
 provider "aws" {
 }
