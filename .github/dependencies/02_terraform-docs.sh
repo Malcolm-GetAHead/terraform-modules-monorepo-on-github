ccf34c0012be0aea6162b0cc84206f8781f672bd
Malcolm Acker <Malcolm@GetA-Head.com>
2024-09-11 10:41:20 -0400

    a

--- .github/dependencies/02_terraform-docs.sh
+++ .github/dependencies/02_terraform-docs.sh
@@ -3,6 +3,6 @@
 VERSION=v0.18.0
 
 curl -sL -o - "$(curl -s https://api.github.com/repos/terraform-docs/terraform-docs/releases/tags/$VERSION | jq -r '.assets[] | select(.name | endswith("-linux-amd64.tar.gz")) | .browser_download_url')" | \
-    tar -C /usr/local/bin -xzf - terraform-docs
+    tar -C "${WS_DIR}/.local/bin" -xzf - terraform-docs
 
 which terraform-docs
