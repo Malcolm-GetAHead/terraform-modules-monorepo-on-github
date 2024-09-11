53fc0190d5a112597014c2582e609360a3a62d5c
Malcolm Acker <Malcolm@GetA-Head.com>
2024-09-11 10:37:59 -0400

    a

--- .github/dependencies/02_terraform.sh
+++ .github/dependencies/02_terraform.sh
@@ -2,9 +2,9 @@
 
 VERSION=1.9.5
 
-curl -o /tmp/terraform.zip "https://releases.hashicorp.com/terraform/${VERSION}/terraform_${VERSION}_linux_amd64.zip"
+curl -o terraform.zip "https://releases.hashicorp.com/terraform/${VERSION}/terraform_${VERSION}_linux_amd64.zip"
 unzip -j -d "${WS_DIR}/.local/bin" terraform.zip
-rm tmp/terraform.zip
-mkdir "$TF_PLUGIN_CACHE_DIR"
+rm terraform.zip
+mkdir -p "$TF_PLUGIN_CACHE_DIR"
 
 which terraform
