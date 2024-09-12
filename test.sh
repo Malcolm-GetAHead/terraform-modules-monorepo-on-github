685f4ec2273eb59adee0c66901c1eb050a12874c
Malcolm Acker <Malcolm@GetA-Head.com>
2024-09-12 13:21:41 -0400

    a

--- test.sh
+++ test.sh
@@ -1,12 +1,4 @@
-declare -A test
 
-test["abc"]=$(terraform-docs markdown table --sort-by required --sort-by name ./test-module)
-test["def"]=$(terraform-docs markdown table --sort-by required --sort-by name ./test-module1)
+a='{"test":{"prevVer":"0.0.0", "newVer": "1.0.0"},"test1":{"prevVer":"0.0.0", "newVer": "1.0.0"}}'
 
-# Prepare the input for jq
-json=$(for key in "${!test[@]}"; do
-  jq -n --arg key "$key" --arg value "${test[$key]}" \
-    '{($key): $value}'
-done | jq -rcs 'add')
-
-echo "$json"
+jq -r '."test.prevVer' <<< "$a"
