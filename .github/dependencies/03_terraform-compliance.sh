997ab979838c0b28bd4a385f1c0a4f5daa4346f5
Malcolm Acker <Malcolm@GetA-Head.com>
2024-09-11 20:51:15 -0400

    dir

--- .github/dependencies/03_terraform-compliance.sh
+++ .github/dependencies/03_terraform-compliance.sh
@@ -3,7 +3,7 @@
 VERSION=1.3.48
 
 python -m venv "$WS_DIR/.cache/terraform-compliance"
-"$WS_DIR/.cache/checkov/bin/pip" install "terraform-compliance==$VERSION"
+"$WS_DIR/.cache/terraform-compliance/bin/pip" install "terraform-compliance==$VERSION"
 
 ln -s "$WS_DIR/.cache/terraform-compliance/bin/terraform-compliance" "${WS_DIR}/.local/bin"
 
