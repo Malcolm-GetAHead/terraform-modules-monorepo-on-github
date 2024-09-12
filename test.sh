21ca4d601d58c2d384696ad1b9db250abf98b373
Malcolm Acker <Malcolm@GetA-Head.com>
2024-09-12 11:07:40 -0400

    debug msgs

--- test.sh
+++ test.sh
@@ -2,12 +2,11 @@ declare -A test
 
 test["abc"]=$(terraform-docs markdown table --sort-by required --sort-by name ./test-module)
 test["def"]=$(terraform-docs markdown table --sort-by required --sort-by name ./test-module1)
-test["ghi"]='"g789h'
 
 # Prepare the input for jq
 json=$(for key in "${!test[@]}"; do
   jq -n --arg key "$key" --arg value "${test[$key]}" \
     '{($key): $value}'
-done | jq -s 'add')
+done | jq -rcs 'add')
 
 echo "$json"
